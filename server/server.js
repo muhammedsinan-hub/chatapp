1cf4a9d28246fb3ef03c67ab289efcb93136e639
= <manchesterisblue1047@gmail.com>
2026-02-13 20:25:52 +0530

    update

--- server/server.js
+++ server/server.js
@@ -48,5 +48,11 @@ app.use("/api/messages", messageRouter);
 //Connect to mongodb
 await connectDB();
 
+
+if(process.env.NODE_ENV !== "production"){
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log("Server is running on port " + PORT));
+server.listen(PORT, () => console.log("Server is running on port " + PORT));}
+
+
+//Export server for vercel
+export default server;
