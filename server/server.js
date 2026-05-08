570add405a813e9fde92d01baabea91ba5ee4c00
muhammedsinan-hub <pirlo21nbnbnb@gmail.com>
2026-05-08 16:22:04 +0530

    fix render

--- server/server.js
+++ server/server.js
@@ -49,9 +49,11 @@ app.use("/api/messages", messageRouter);
 await connectDB();
 
 
-if(process.env.NODE_ENV !== "production"){
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log("Server is running on port " + PORT));}
+
+server.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
 
 
 //Export server for vercel
