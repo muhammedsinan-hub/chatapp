829e7b5bb5f2e6e84c9e18c9dbb69c1fa2346a30
muhammedsinan-hub <pirlo21nbnbnb@gmail.com>
2026-05-29 19:03:27 +0530

    loading screen logo added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import LoginPage from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
 import { Toaster } from "react-hot-toast";
 import { AuthContext } from "../context/AuthContext";
-import { assets } from "./assets/assets";
+import assets from "./assets/assets";
 
 function App() {
 
