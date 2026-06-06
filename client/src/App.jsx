d014f366a840b5d05195f061b1df04fe5f620354
muhammedsinan-hub <pirlo21nbnbnb@gmail.com>
2026-06-06 19:54:58 +0530

    loading screen fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,64 +1,27 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
 import { Toaster } from "react-hot-toast";
 import { AuthContext } from "../context/AuthContext";
-import assets from "./assets/assets";
 
 function App() {
-
   const { authUser, isCheckingAuth } = useContext(AuthContext);
 
-  const [showLoader, setShowLoader] = useState(true);
-
-  useEffect(() => {
-
-    if (!isCheckingAuth) {
-      setTimeout(() => {
-        setShowLoader(false);
-      }, 3000);
-    }
-
-  }, [isCheckingAuth]);
-
-  if (isCheckingAuth || showLoader) {
+  if (isCheckingAuth) {
     return (
-<div className="min-h-screen w-full flex flex-col justify-center items-center px-4 text-center bg-[#1a1a1a]">
-        {/* Logo */}
-        <img
-          src={assets.logo}
-          alt="Logo"
-         className="w-28 sm:w-32 mb-4 mx-auto translate-x-3" />
-
-        {/* Welcome Text */}
-        <h1 className="text-2xl sm:text-3xl font-bold text-white">
- Welcome to QuickChat        </h1>
-
-        <p className="text-sm text-gray-400 mt-2 mb-8">
-          Preparing your chat experience...
-        </p>
-
-        {/* Spinner */}
+      <div className="min-h-screen flex items-center justify-center bg-[#1a1a1a]">
         <div className="w-12 h-12 border-4 border-t-purple-500 border-gray-700 rounded-full animate-spin"></div>
-
-        {/* Bottom Message */}
-        <p className="text-sm text-gray-400 mt-4">
-          Please wait while we connect everything...
-        </p>
-
       </div>
     );
   }
 
   return (
     <div className="bg-[#1a1a1a] bg-[url('/bgImage.svg')] bg-cover min-h-screen">
-
       <Toaster />
 
       <Routes>
-
         <Route
           path="/"
           element={authUser ? <HomePage /> : <Navigate to="/login" />}
@@ -75,9 +38,7 @@ function App() {
         />
 
         <Route path="*" element={<Navigate to="/" />} />
-
       </Routes>
-
     </div>
   );
 }
